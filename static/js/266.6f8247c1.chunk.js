36447e9da7ac96de268bac3a0a4378203ac30bee
OleksiiDmytruk <125202905+OleksiiDmytruk@users.noreply.github.com>
2023-10-27 00:02:11 +0000

    Deploying to gh-pages from @ OleksiiDmytruk/goit-react-hw-08-phonebook@69865fc4cacaea7cc58e496b36ed5c876b692617 🚀

---
+++ static/js/266.6f8247c1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_hw_08_phonebook=self.webpackChunkreact_hw_08_phonebook||[]).push([[266],{1266:function(e,o,n){n.r(o),n.d(o,{Home:function(){return c}});var r=n(9434),h=n(3646),t=n(184),c=function(){var e=(0,r.v9)(h.Qb);return(0,t.jsxs)("div",{children:[(0,t.jsx)("h2",{children:"Hello. Welcome to the phone book. "}),e?(0,t.jsx)("h3",{children:"Thank you for using our application"}):(0,t.jsx)("h3",{children:" To start work, register or log in"})]})};o.default=c}}]);
+//# sourceMappingURL=266.6f8247c1.chunk.js.map
